253b43288fa0533e71f24980a34e28fd1d7f0feb
sharadtajne <sharad.tajne2023@ssipmt.com>
2026-02-19 14:13:23 +0530

    Add files via upload

---
+++ aoc.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int r;
+    cout << "The radius of the circle is : ";
+    cin >> r;
+
+    float circle;
+    circle = 3.14 * r * r;
+
+    cout << circle << endl;
+    return 0;
+}
